2247a8db8c46958caec04d847fd500bba43526b2
François Laurent <francois.laurent@mail.ru>
2017-04-26 00:44:34 +0200

    version number incremented

--- doc/conf.py
+++ doc/conf.py
@@ -83,7 +83,7 @@ copyright = u'2017, ' + author
 # built documents.
 #
 # The short X.Y version.
-version = u'0.3.1'
+version = u'0.3.2'
 # The full version, including alpha/beta/rc tags.
 release = version
 
